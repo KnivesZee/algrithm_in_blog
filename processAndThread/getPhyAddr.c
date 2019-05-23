93b4583f77e0267f62b4bc0cae6bea5944241162
knvies <kniveszee@gmail.com>
2019-05-23 20:57:56 +0800

    获取变量的实际内存地址

--- processAndThread/getPhyAddr.c
+++ processAndThread/getPhyAddr.c
@@ -1,47 +1,58 @@
-// 获取变量物理地址
-#include <stdio.h>
-#include <unistd.h>
-#include <inttypes.h>
-#include <stdint.h>
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <fcntl.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-#define page_map_file "/proc/self/pagemap"
 
-#define PFN_MASK ((((uint64_t)1)<<55)-1)
+#include <stdint.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 
-#define PFN_PRESENT_FLAG (((uint64_t)1)<<63)
 
-int mem_addr_vir2phy(unsigned long vir, unsigned long *phy)
+
+#define    page_map_file     "/proc/self/pagemap"
+#define    PFN_MASK          ((((uint64_t)1)<<55)-1)
+#define    PFN_PRESENT_FLAG  (((uint64_t)1)<<63)
+
+
+unsigned long mem_addr_vir2phy(unsigned long vir)
 {
-    int fd;
-    int page_size=getpagesize();
-    unsigned long vir_page_idx = vir/page_size;
-    unsigned long pfn_item_offset = vir_page_idx*sizeof(uint64_t);
-    uint64_t pfn_item;
+        long fd;
+            long page_size = getpagesize();
+                unsigned long vir_page_idx = vir/page_size;
+                    unsigned long pfn_item_offset = vir_page_idx*sizeof(uint64_t);
+                        uint64_t pfn_item;
+                            unsigned long phyaddr;
 
-    fd = open(page_map_file, O_RDONLY);
-    if (fd<0)
-    {
-        printf("open %s failed", page_map_file);
-        return -1;
-    }
-    if ((off_t)-1 == lseek(fd, pfn_item_offset, SEEK_SET))
-    {
-        printf("lseek %s failed", page_map_file);
-        return -1;
-    }
-    if (sizeof(uint64_t) != read(fd, &pfn_item, sizeof(uint64_t)))
-    {
-        printf("read %s failed", page_map_file);
-        return -1;
-    }
-    if (0==(pfn_item & PFN_PRESENT_FLAG))
-    {
-        printf("page is not present");
-        return -1;
-    }
-    *phy = (pfn_item & PFN_MASK)*page_size + vir % page_size;
-    return 0;
+                                fd = open(page_map_file, O_RDONLY);
+                                    if (fd<0)
+                                            {
+                                                        printf("open %s failed", page_map_file);
+                                                                return -1;
+                                                                    }
+
+
+                                        if ((off_t)-1 == lseek(fd, pfn_item_offset, SEEK_SET))
+                                                {
+                                                            printf("lseek %s failed", page_map_file);
+                                                                    return -1;
+                                                                        }
+
+
+                                            if (sizeof(uint64_t) != read(fd, &pfn_item, sizeof(uint64_t)))
+                                                    {
+                                                                printf("read %s failed", page_map_file);
+                                                                        return -1;
+                                                                            }
+
+
+                                                if (0==(pfn_item & PFN_PRESENT_FLAG))
+                                                        {
+                                                                    printf("page is not present");
+                                                                            return -1;
+                                                                                }
+
+
+                                                    phyaddr = (pfn_item & PFN_MASK)*page_size + vir % page_size;
+                                                        return phyaddr;
 }
